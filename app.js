3e28830f6153f27c146a7ff6868632a752298d8e
Omar <124088826+oali220@users.noreply.github.com>
2023-02-25 17:01:00 -0600

    Updated styling/layout with HTML/CSS/Bootstrap

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
-function makeDisplay() {
-    let calcContainer = document.querySelector(".container");
+// function makeDisplay() {
+//     let calcContainer = document.querySelector(".container");
 
-    calcContainer.style.gridTemplateColumns = `repeat(4, 1fr)`;
-    calcContainer.style.gridTemplateRows = `repeat(6, 1fr)`;
-}
+//     calcContainer.style.gridTemplateColumns = `repeat(4, 1fr)`;
+//     calcContainer.style.gridTemplateRows = `repeat(6, 1fr)`;
+// }
 
-makeDisplay();
+
