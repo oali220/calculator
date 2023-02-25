6e9aea8128115b58943ecdf26fe4ca2ed277863d
Omar <124088826+oali220@users.noreply.github.com>
2023-02-25 14:14:34 -0600

    completed grid layout

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function makeDisplay() {
+    let calcContainer = document.querySelector(".container");
+
+    calcContainer.style.gridTemplateColumns = `repeat(4, 1fr)`;
+    calcContainer.style.gridTemplateRows = `repeat(6, 1fr)`;
+}
+
+makeDisplay();
