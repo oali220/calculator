f277ac09deff4f874423c1439e85d58169360785
Omar <124088826+oali220@users.noreply.github.com>
2023-02-26 17:02:05 -0600

    Carries out operations, displays on screen

--- app.js
+++ app.js
@@ -1,9 +1,79 @@
+let operator = '';
+let currentVal = '';
+let previousVal = '';
+let result = '';
 
-// function makeDisplay() {
-//     let calcContainer = document.querySelector(".container");
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
 
-//     calcContainer.style.gridTemplateColumns = `repeat(4, 1fr)`;
-//     calcContainer.style.gridTemplateRows = `repeat(6, 1fr)`;
-// }
+let clear = document.querySelector(".clear");
+let decimal = document.querySelector(".decimal");
+let equal = document.querySelector(".equal");
+
+let previous = document.querySelector(".previous");
+let current = document.querySelector(".current");
 
 
+numbers.forEach((number) => number.addEventListener('click', function(e) {
+    getValue(e.target.textContent)
+    current.textContent = currentVal;
+}))
+
+operators.forEach((oper) => oper.addEventListener('click', function(e) {
+    getOperator(e.target.textContent)
+    previous.textContent = previousVal + ' ' + operator;
+    current.textContent = currentVal;
+}))
+
+clear.addEventListener('click', function() {
+    operator = '';
+    currentVal = '';
+    previousVal = '';
+    previous.textContent = previousVal + ' ' + operator;
+    current.textContent = currentVal;
+})
+
+equal.addEventListener('click', function() {
+    calculate(currentVal, previousVal, operator);
+    current.textContent = result;
+    previous.textContent = previousVal + ' ' + operator;
+})
+
+function getValue(num) {
+    if(currentVal.length < 6) {
+    currentVal += num;
+    }
+}
+
+function getOperator(oper) {
+    operator = oper;
+    previousVal = currentVal;
+    currentVal = '';
+}
+
+function calculate(firstVal, secondVal, oper) {
+    firstVal = Number(firstVal);
+    secondVal = Number(secondVal);
+
+    if(oper == '*') {
+        result = firstVal * secondVal;
+    } 
+    else if (oper === '/') 
+    {
+        result = firstVal / secondVal;
+    } 
+    else if (oper === '-') 
+    {
+        result = firstVal - secondVal;
+    } 
+    else if (oper === '+') 
+    {
+        result = firstVal + secondVal;
+    }
+
+    result = Math.round(result * 10000) / 10000;
+
+    previousVal = '';
+    currentVal = result;
+    operator = '';
+}
